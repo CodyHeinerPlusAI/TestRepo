c68bb05c1feae343df3f6c9f384a38c6e0aaccd2
Apollys <cody.heiner@plus.ai>
2019-05-02 11:45:59 -0700

    Initialize test repo

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int Factorial(int n) {
+  int result = 1;
+  while (n > 1) {
+    result *= n;
+    --n;
+  }
+  return result;
+}
+
+int main() {
+  std::cout << "3! = " << Factorial(3) << std::endl;
+  
+  return 0;
+}
